a4c149d8cca3beab9fabf07fca11f4cf98781d9b
krtoca <krtoca@gmail.com>
2026-05-08 14:00:47 -0400

    Update

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,6 +1,9 @@
 import { Outlet, useLoaderData, useRouteError } from "react-router";
 import { boundary } from "@shopify/shopify-app-react-router/server";
-import { AppProvider } from "@shopify/shopify-app-react-router/react";
+import { AppProvider as ShopifyAppProvider } from "@shopify/shopify-app-react-router/react";
+import { AppProvider as PolarisAppProvider } from "@shopify/polaris";
+import enTranslations from "@shopify/polaris/locales/en.json";
+import "@shopify/polaris/build/esm/styles.css";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
@@ -13,15 +16,17 @@ export default function App() {
   const { apiKey } = useLoaderData();
 
   return (
-    <AppProvider embedded apiKey={apiKey}>
-      <s-app-nav>
-        <s-link href="/app">Home</s-link>
-        <s-link href="/app/settings">Settings</s-link>
-        <s-link href="/app/test-email">Test Email</s-link>
-      </s-app-nav>
+    <ShopifyAppProvider embedded apiKey={apiKey}>
+      <PolarisAppProvider i18n={enTranslations}>
+        <s-app-nav>
+          <s-link href="/app">Home</s-link>
+          <s-link href="/app/settings">Settings</s-link>
+          <s-link href="/app/test-email">Test Email</s-link>
+        </s-app-nav>
 
-      <Outlet />
-    </AppProvider>
+        <Outlet />
+      </PolarisAppProvider>
+    </ShopifyAppProvider>
   );
 }
 
